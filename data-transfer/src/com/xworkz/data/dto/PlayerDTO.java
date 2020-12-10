4b492a8f98228cc46610d89c49dc0fb1f13dd038
surakshita <72905080+surakshita@users.noreply.github.com>
2020-12-10 08:47:39 +0530

    DTO

---
+++ data-transfer/src/com/xworkz/data/dto/PlayerDTO.java
@@ -0,0 +1,40 @@
+package com.xworkz.data.dto;
+
+public class PlayerDTO {
+	private String name;
+	private String[] sports;
+	private String country;
+	private String[] awards;
+	
+	
+	public PlayerDTO()
+	{
+		System.out.println("created player");
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String[] getSports() {
+		return sports;
+	}
+	public void setSports(String[] sports) {
+		this.sports = sports;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String[] getAwards() {
+		return awards;
+	}
+	public void setAwards(String[] awards) {
+		this.awards = awards;
+	}
+	
+	
+}
